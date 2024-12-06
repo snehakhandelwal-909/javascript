a9562d6360523e68bb2d9b43dcde2689d4eed71d
snehakhandelwal-909 <snehakhandelwal2k04@gmail.com>
2024-12-06 15:28:27 +0000

    data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+let name="sneha"
+let age=20
+let isloggedIn=false
+//primitive
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=> matlab abhi value assign nahi hui hai
+//symbol=>unique
+
+//objects
+
+console.log(typeof "sneha")
+console.log(typeof age);
+console.log(typeof undefined)
+console.log(typeof null);
+
+
+
+
